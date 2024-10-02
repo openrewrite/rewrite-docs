eca558fdc1c415b27fe86812110432701a206539
Mike Solomon <mike-solomon@users.noreply.github.com>
2024-10-02 10:21:21 -0700

    Update workflows for github pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,15 +21,17 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://openrewrite.github.io/rewrite-docs',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'mike-solomon', // Usually your GitHub org/user name.
-  projectName: 'test-docusaurus', // Usually your repo name.
+  organizationName: 'openrewrite', // Usually your GitHub org/user name.
+  projectName: 'rewrite-docs', // Usually your repo name.
+  deploymentBranch: 'docusaurus',
+  trailingSlash: false,
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
