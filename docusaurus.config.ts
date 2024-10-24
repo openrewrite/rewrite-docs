7c65044f8df53a543b168c9371a5e06fb4efbb07
Mike Solomon <mike-solomon@users.noreply.github.com>
2024-10-24 09:54:48 -0700

    Adjust dark mode theme

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -166,7 +166,7 @@ const config: Config = {
     },
     prism: {
       theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
+      darkTheme: prismThemes.vsDark,
       additionalLanguages: [
         "bash",
         "docker",
