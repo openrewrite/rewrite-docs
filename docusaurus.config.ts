3ed698406bb4116f9f9a8ea2c78746584312a2fb
Mike Solomon <mike-solomon@users.noreply.github.com>
2025-05-14 07:49:20 -0700

    Remove code remix banner

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -124,11 +124,11 @@ const config: Config = {
       apiKey: "14363fc8de10ed3d1785fe6d17a92631",
       indexName: "openrewrite",
     },
-    announcementBar: {
-      id: "code_remix",
-      content:
-        'Now announcing the inaugural <a href="https://coderemix.ai/"><strong>Code Remix Summit</strong></a> – in Miami May 12th-14th. Use the code <strong>MODERNE3VIP</strong> for an additional $300 off.',
-    },
+    // announcementBar: {
+    //   id: "code_remix",
+    //   content:
+    //     'Now announcing the inaugural <a href="https://coderemix.ai/"><strong>Code Remix Summit</strong></a> – in Miami May 12th-14th.',
+    // },
     image: "img/moderne-poster-logo.svg",
     colorMode: {
       respectPrefersColorScheme: true,
