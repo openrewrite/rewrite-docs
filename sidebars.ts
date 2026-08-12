bfe728b2c1731ba962942cc11eb0395be24ed5b2
Tim te Beek <tim@moderne.io>
2026-08-12 21:55:36 +0200

    Add the 8.89.0 changelog (#529)

--- sidebars.ts
+++ sidebars.ts
@@ -192,6 +192,7 @@ const sidebars: SidebarsConfig = {
         keywords: ['changelog'],
       },
       items: [
+        'changelog/8-89-0-Release',
         'changelog/8-88-0-Release',
         'changelog/8-87-0-Release',
         'changelog/8-86-0-Release',
